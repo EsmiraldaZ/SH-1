7e95ff1a6d647c4fc93423199e2bbe6f26f6146d
EsmiraldaZ <32571297+EsmiraldaZ@users.noreply.github.com>
2018-08-21 17:29:23 +0300

    Update lamp.js

--- lamp.js
+++ lamp.js
@@ -1,19 +1,17 @@
 "use strict";
 function Lamp(name){
     Device.call(this, "lamp", name);
-    this._currentBright = 50;
-    this._memoryBright = 0;
-    this.MAX_BRIGT = 100;
-    this.MIN_BRIGT = 0;
-}
-
+    this._basicBright = Lamp.MIN_BRIGHT;
+    
+Lamp.MAX_BRIGT = 100;
+Lamp.MIN_BRIGT = 0;
 Lamp.prototype = Object.create(Device.prototype);
 Lamp.prototype.constructor = Lamp;
  
-Lamp.prototype.getCurrentBright = function(){
-	 return this._currentBright; 
+Lamp.prototype.getBasicBright = function(){
+	 return this._basictBright; 
 	
-Lamp.prototype.setCurrentBright = function(value){
+Lamp.prototype.setBasicBright = function(value){
      if (value >= Lamp.MIN_BRIGHT && value <= Lamp.MAX_BRIGHT) {
         this._power = value;
     }
@@ -21,13 +19,13 @@ Lamp.prototype.setCurrentBright = function(value){
 }
 
 Lamp.prototype.increaseBright = function(){
-	if(this._currentBright < this.MAX_BRIGT){
+	if(this._basictBright < Lamp.MAX_BRIGT){
 		this._currentBright++;
 	}
 }
 
 Lamp.prototype.decreaseBright = function(){
-	if(this._currentBright > this.MIN_BRIGT){
-		this._currentBright--;
+	if(this._basicBright > Lamp.MIN_BRIGT){
+		this._basicBright--;
 	}
 }
